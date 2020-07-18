92fa506453acb44faa739a74ff3cc8b8d37d8d89
Chelese Jensen <jensenchelese@gmail.com>
2020-07-18 12:22:49 -0700

    Update to ignore script

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const nr = require('newrelic');
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
